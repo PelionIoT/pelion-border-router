16ceaf29d9ed483f0d17744c286867f04d99c4a2
Teemu Takaluoma <teemu.takaluoma@pelion.com>
2021-09-21 11:22:06 +0300

    pelion-border-router 2.3.0

--- main_app.cpp
+++ main_app.cpp
@@ -26,6 +26,7 @@
 #include "MeshInterfaceNanostack.h"
 #include "network_dns_optimization.h"
 #include "cloud_client_helper.h"
+#include "kvstore_global_api.h"
 #if defined MBED_CONF_MBED_CLOUD_CLIENT_NETWORK_MANAGER && (MBED_CONF_MBED_CLOUD_CLIENT_NETWORK_MANAGER == 1)
 #include "NetworkManager.h"
 #endif
@@ -58,6 +59,22 @@
 #define BACKHAUL_CONNECTION_RETRY_TIMEOUT_MAX 300*1000
 #endif
 
+#ifndef MESH_IFACE_START_MAX_WAIT
+#ifdef MBED_CONF_APP_MESH_IFACE_START_TIMEOUT
+#define MESH_IFACE_START_MAX_WAIT (MBED_CONF_APP_MESH_IFACE_START_TIMEOUT*1000)
+#else
+#define MESH_IFACE_START_MAX_WAIT 10*60*1000   // 10 Minutes
+#endif
+#endif
+
+#define MESH_IFACE_CTRL_VAL_MAX_SIZE        16
+#define APP_STATE_VAL_MAX_SIZE              32
+#define MESH_IFACE_CTRL_CONTINUE            "CONTINUE"
+#define MESH_IFACE_CTRL_BLOCK               "BLOCK"
+#define APP_STATE_WAIT_PERMISSION           "Waiting Permission"
+#define APP_STATE_WISUN_BOOTING             "Wi-SUN Booting"
+#define APP_STATE_WISUN_ACTIVE              "Wi-SUN Active"
+
 static NetworkInterface *backhaul_interface = NULL;
 static WisunInterface *mesh_interface = NULL;
 static WisunBorderRouter ws_border_router;
@@ -72,8 +89,19 @@ static M2MResource *m2m_put_res;
 static M2MResource *m2m_post_res;
 static M2MResource *m2m_deregister_res;
 static M2MResource *m2m_factory_reset_res;
+static M2MResource *mesh_iface_control;
+static M2MResource *app_state;
+
+static void mesh_connect(void);
+static void check_mesh_iface_control(void);
+static char app_mesh_control_kv_key[] = "/kv/mesh_iface_ctrl_key";
+static char mesh_iface_control_value[MESH_IFACE_CTRL_VAL_MAX_SIZE] = {0, };
+static char app_state_value[APP_STATE_VAL_MAX_SIZE] = {0, };
+static bool mesh_interface_up = false;
+rtos::Semaphore mesh_control_data_found;
 
 static EventQueue *queue;
+static EventQueue *mesh_iface_control_queue;
 
 #if defined MBED_CONF_APP_ACTIVE_KEEP_ALIVE && (MBED_CONF_APP_ACTIVE_KEEP_ALIVE == 1)
 static EventQueue *keep_alive_queue;
@@ -92,8 +120,6 @@ typedef enum app_status {
     APP_STATUS_FAIL = -1,
     APP_STATUS_SUCCESS = 0
 } app_status_t;
-
-Mutex value_increment_mutex;
 
 #if defined MBED_CONF_APP_MEM_STATS_PERIODIC_TRACE && (MBED_CONF_APP_MEM_STATS_PERIODIC_TRACE == 1)
 static void print_ns_heap_stats(void)
@@ -148,19 +174,23 @@ static void client_registered(void)
     printf("Client registered\n");
     print_client_ids();
 
+    if (!mesh_interface_up) {
+        check_mesh_iface_control();
+    } else {
 #if defined MBED_CONF_MBED_CLOUD_CLIENT_NETWORK_MANAGER && (MBED_CONF_MBED_CLOUD_CLIENT_NETWORK_MANAGER == 1)
-    ws_network_manager.nm_cloud_client_connect_indication();
+        ws_network_manager.nm_cloud_client_connect_indication();
 #endif
 
 #if defined(MBED_CONF_APP_WISUN_NETWORK_DNS_OPTIMIZATION) && (MBED_CONF_APP_WISUN_NETWORK_DNS_OPTIMIZATION == 1)
-    network_dns_opt_configure(&ws_border_router, backhaul_interface);
-    network_dns_opt_query_set();
+        network_dns_opt_configure(&ws_border_router, backhaul_interface);
+        network_dns_opt_query_set();
 #if MBED_MAJOR_VERSION > 5
-    queue->call_every(std::chrono::milliseconds(DNS_QUERY_SET_PERIOD), network_dns_opt_query_set);
+        queue->call_every(std::chrono::milliseconds(DNS_QUERY_SET_PERIOD), network_dns_opt_query_set);
 #else
-    queue->call_every(DNS_QUERY_SET_PERIOD, network_dns_opt_query_set);
+        queue->call_every(DNS_QUERY_SET_PERIOD, network_dns_opt_query_set);
 #endif
 #endif
+    }
 }
 
 static void client_unregistered(void)
@@ -173,11 +203,13 @@ static void client_error(int err)
     tr_err("client_error(%d) -> %s", err, cloud_client->error_description());
 }
 
+#ifdef MBED_CLOUD_CLIENT_SUPPORT_UPDATE
 static void update_progress(uint32_t progress, uint32_t total)
 {
     uint8_t percent = (uint8_t)((uint64_t)progress * 100 / total);
     tr_info("Update progress = %" PRIu8 "%%", percent);
 }
+#endif
 
 static void get_res_update(const char * /*object_name*/)
 {
@@ -210,8 +242,105 @@ static void factory_reset(void *)
     kcm_factory_reset();
 }
 
+static void mesh_iface_start_max_wait_cb(void)
+{
+    mesh_control_data_found.release();
+}
+
+static void check_mesh_iface_control(void)
+{
+    // if the mesh_iface_control_value is not MESH_IFACE_CTRL_BLOCK, then release the mesh_control_data_found semaphore
+    if (strncmp(mesh_iface_control_value, MESH_IFACE_CTRL_BLOCK, strlen(MESH_IFACE_CTRL_BLOCK))) {
+        mesh_control_data_found.release();
+    } else {
+        strcpy(app_state_value, APP_STATE_WAIT_PERMISSION);
+        tr_warn("Waiting for mesh_iface_control_value to start the mesh interface");
+        if (MESH_IFACE_START_MAX_WAIT != 0) {
+#if MBED_MAJOR_VERSION > 5
+            mesh_iface_control_queue->call_in(std::chrono::milliseconds(MESH_IFACE_START_MAX_WAIT), mesh_iface_start_max_wait_cb);
+#else
+            mesh_iface_control_queue->call_in(MESH_IFACE_START_MAX_WAIT, mesh_iface_start_max_wait_cb);
+#endif
+        }
+    }
+}
+
+static void mesh_iface_control_cb(const char * /*object_name*/)
+{
+    int kv_status;
+    String mesh_iface_cval = mesh_iface_control->get_value_string();
+
+    if (mesh_iface_cval.c_str() == NULL) {
+        tr_error("Received Mesh Control Data is NULL\n");
+        return;
+    }
+
+    if (strlen((char *)mesh_iface_cval.c_str()) <= MESH_IFACE_CTRL_VAL_MAX_SIZE) {
+        strcpy(mesh_iface_control_value, (char *)mesh_iface_cval.c_str());
+    } else {
+        strncpy(mesh_iface_control_value, (char *)mesh_iface_cval.c_str(), MESH_IFACE_CTRL_VAL_MAX_SIZE - 1);
+    }
+
+    tr_info("Received Mesh Control Data: %s", mesh_iface_control_value);
+
+    /* Set Key/Value pair with unprotected clear value data */
+    tr_debug("Setting mesh_iface_control_value in KVStore using key: %s\n", app_mesh_control_kv_key);
+    kv_status = kv_set(app_mesh_control_kv_key, mesh_iface_control_value, strlen((char *)mesh_iface_cval.c_str()) + 1, 0);
+    if (kv_status != MBED_SUCCESS) {
+        tr_warn("Could not set Mesh Control Data into KVStore, Error: %d", MBED_GET_ERROR_CODE(kv_status));
+    } else {
+        tr_info("Mesh Control Data stored into KVStore");
+    }
+
+    if (!mesh_interface_up) {
+        // if the received value is not MESH_IFACE_CTRL_BLOCK, then release the mesh_control_data_found semaphore
+        if (strncmp(mesh_iface_control_value, MESH_IFACE_CTRL_BLOCK, strlen(MESH_IFACE_CTRL_BLOCK))) {
+            mesh_control_data_found.release();
+        } else {
+            tr_warn("Mesh interface is not started");
+        }
+    }
+}
+
+static coap_response_code_e app_res_read_cb(const M2MResourceBase &resource,
+                                            uint8_t *&buffer,
+                                            size_t &buffer_size,
+                                            size_t &total_size,
+                                            const size_t offset,
+                                            void *client_args)
+{
+    tr_info("GET request received for resource: %s", resource.uri_path());
+
+    M2MBase *obj = (M2MBase *)client_args;
+    if (obj == mesh_iface_control) {
+        buffer = (uint8_t *)mesh_iface_control_value;
+        buffer_size = strlen(mesh_iface_control_value);
+        tr_debug("Setting mesh_iface_control_value to Client: %s", mesh_iface_control_value);
+    }
+
+    if (obj == app_state) {
+        buffer = (uint8_t *)app_state_value;
+        buffer_size = strlen(app_state_value);
+        tr_debug("Setting app_state_value to Client: %s", app_state_value);
+    }
+
+    return COAP_RESPONSE_CONTENT;
+}
+
 static app_status_t PDMC_create_resource(void)
 {
+    // PUT/GET resource 33455/0/13
+    mesh_iface_control = M2MInterfaceFactory::create_resource(m2m_obj_list, 33455, 0, 13, M2MResourceInstance::STRING, M2MBase::GET_PUT_ALLOWED);
+    if (mesh_iface_control->set_value_updated_function(mesh_iface_control_cb) != true) {
+        tr_error("mesh_iface_control->set_value_updated_function() failed");
+        return APP_STATUS_FAIL;
+    }
+    mesh_iface_control->set_read_resource_function(app_res_read_cb, mesh_iface_control);
+
+    // GET resource 33455/0/14
+    app_state = M2MInterfaceFactory::create_resource(m2m_obj_list, 33455, 0, 14, M2MResourceInstance::STRING, M2MBase::GET_ALLOWED);
+    app_state->set_read_resource_function(app_res_read_cb, app_state);
+
     // GET resource 3200/0/5501
     // PUT also allowed for resetting the resource
     m2m_get_res = M2MInterfaceFactory::create_resource(m2m_obj_list, 3200, 0, 5501, M2MResourceInstance::INTEGER, M2MBase::GET_PUT_ALLOWED);
@@ -336,12 +465,14 @@ static void mesh_interface_status_callback(nsapi_event_t status, intptr_t param)
     if (status == NSAPI_EVENT_CONNECTION_STATUS_CHANGE) {
         switch (param) {
             case NSAPI_STATUS_GLOBAL_UP:
-                tr_info("Mesh Interface::NSAPI_STATUS_GLOBAL_UP");
+                printf("Mesh Interface::NSAPI_STATUS_GLOBAL_UP");
                 if (mesh_interface->get_ip_address(&sa) != 0) {
                     tr_err("Could not fetch Global IP of Mesh Interface");
                     break;
                 }
                 tr_info("Mesh Interface connected with IP %s", sa.get_ip_address());
+                mesh_interface_up = true;
+                strcpy(app_state_value, APP_STATE_WISUN_ACTIVE);
                 mesh_global_ip.release();
                 break;
             case NSAPI_STATUS_LOCAL_UP:
@@ -359,6 +490,31 @@ static void mesh_interface_status_callback(nsapi_event_t status, intptr_t param)
                 break;
         }
     }
+}
+
+static void mesh_connect(void)
+{
+    int status;
+    tr_info("Connect to Mesh Interface");
+
+    // Updating App state as APP_STATE_WISUN_BOOTING
+    strcpy(app_state_value, APP_STATE_WISUN_BOOTING);
+
+    mesh_interface->add_event_listener(mbed::callback(&mesh_interface_status_callback));
+    status = mesh_interface->connect();
+    if (status == NSAPI_ERROR_OK || status == NSAPI_ERROR_IS_CONNECTED) {
+        if (backhaul_interface != NULL) {
+            if (ws_border_router.start(mesh_interface, backhaul_interface) != MESH_ERROR_NONE) {
+                printf("FAILED to start Border Router\n");
+                return;
+            }
+        } else {
+            tr_warn("Backhaul Interface is not yet started");
+        }
+    } else {
+        tr_err("Mesh Interface failed to connect with %d", status);
+    }
+    return;
 }
 
 static app_status_t backhaul_connect(void)
@@ -412,6 +568,7 @@ int main(void)
     int status;
 
     queue = mbed_event_queue();
+    mesh_iface_control_queue = mbed_event_queue();
 
 #if defined MBED_CONF_APP_ACTIVE_KEEP_ALIVE && (MBED_CONF_APP_ACTIVE_KEEP_ALIVE == 1)
     keep_alive_queue = mbed_event_queue();
@@ -492,19 +649,15 @@ int main(void)
     }
 #endif
 
-    tr_info("Connect to Mesh Interface");
-    mesh_interface->add_event_listener(mbed::callback(&mesh_interface_status_callback));
-    status = mesh_interface->connect();
-    if (status == NSAPI_ERROR_OK || status == NSAPI_ERROR_IS_CONNECTED) {
-        if (ws_border_router.start(mesh_interface, backhaul_interface) != MESH_ERROR_NONE) {
-            printf("FAILED to start Border Router\n");
-            return -1;
-        }
-    } else {
-        tr_err("Mesh Interface failed to connect with %d", status);
-        return -1;
+    // Read default Mesh Interface Control
+#ifdef MBED_CONF_APP_MESH_IFACE_START_CONTROL
+    size_t actual_size = 0;
+    strncpy(mesh_iface_control_value, MBED_CONF_APP_MESH_IFACE_START_CONTROL, strlen(MBED_CONF_APP_MESH_IFACE_START_CONTROL));
+    kv_get(app_mesh_control_kv_key, mesh_iface_control_value, MESH_IFACE_CTRL_VAL_MAX_SIZE, &actual_size);
+    if (kv_set(app_mesh_control_kv_key, mesh_iface_control_value, actual_size, 0) != MBED_SUCCESS) {
+        tr_error("Could not set Default Mesh Control Data into KVStore");
     }
-    mesh_global_ip.acquire();
+#endif
 
     if (PDMC_create_resource() == APP_STATUS_FAIL) {
         tr_err("Failed to Create Resources: Terminating Application");
@@ -516,12 +669,17 @@ int main(void)
 #endif
 
     cloud_client->add_objects(m2m_obj_list);
+    cloud_client->setup(backhaul_interface);
+
+    mesh_control_data_found.acquire();
+    mesh_connect();
+    mesh_global_ip.acquire();
 
 #ifdef MBED_CLOUD_CLIENT_SUPPORT_MULTICAST_UPDATE
     arm_uc_multicast_interface_configure(get_mesh_iface_id());
 #endif
 
-    cloud_client->setup(backhaul_interface);
+    cloud_client->setup(backhaul_interface, true);
 
 #ifdef MBED_MEM_TRACING_ENABLED
     mbed_mem_trace_set_callback(mbed_mem_trace_default_callback);
